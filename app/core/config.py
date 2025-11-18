12ed3467ceb1d485e7d680a904cc75569e9f0b4b
Oualid OUZANIK <oualid.ouzanik@m2t.biz>
2025-11-18 17:18:34 +0100

    Fix envirement

---
+++ app/core/config.py
@@ -0,0 +1,34 @@
+from pydantic import model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    # General settings
+    APP_NAME: str
+    APP_DESCRIPTION: str
+    APP_VERSION: str
+    BACKEND_CORS_ORIGINS: str
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    @model_validator(mode='after')
+    def assemble_cors_origins(self) -> 'Settings':
+        if isinstance(self.BACKEND_CORS_ORIGINS, str):
+            self.BACKEND_CORS_ORIGINS = [item.strip() for item in self.BACKEND_CORS_ORIGINS.split(',')]
+        return self
+
+    # Database settings
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        return f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+
+settings = Settings()
